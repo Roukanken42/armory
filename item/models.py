a040b15f0f64692c399c55a29d493844610fa146
kuko0411 <kuko0411@gmail.com>
2017-03-26 19:24:45 +0200

    Added ItemData to database

--- item/models.py
+++ item/models.py
@@ -1,32 +1,40 @@
 from django.db import models
 
 # Create your models here.
-class ItemData(models.Model):                   #for parsing purposes
-    type = models.IntegerField()                #id
-    name = models.CharField(max_length=200)     #string
-    tooltip = models.CharField(max_length=500)  #toolTip
+class ItemData(models.Model):                   # comments for parsing purposes
+    type = models.IntegerField()                # id
+    name = models.CharField(max_length=200)     # string
+    tooltip = models.CharField(max_length=500)  # toolTip
+    icon = models.CharField(max_length=200)     
+    
+    tradable = models.BooleanField()
+    warehouseStorable = models.BooleanField()
+    guildWarehouseStorable = models.BooleanField()
+    storeSellable = models.BooleanField()
+    enchantEnable = models.BooleanField()
 
+    boundType = models.CharField(max_length=20)
+    category = models.CharField(max_length=20)
+    combatItemType = models.CharField(max_length=20)
+    requiredEquipmentType = models.CharField(max_length=20)
+
+    level = models.IntegerField()
+    rank = models.IntegerField()
+    rareGrade = models.IntegerField()
+
+    # discutable
 
     # "artisanable": false,
-    # "boundType": "None",
-    # "buyPrice": "10000",
-    # "category": "combat",
-    # "changeColorEnable": false,
-    # "changeLook": false,
-    # "combatItemType": "DISPOSAL",
-    # "coolTime": 30,
-    # "coolTimeGroup": 3,
-    # "destroyable": true,
+    # "unidentifiedItemGrade": 0,
     # "dismantlable": false,
-    # "dropSilhouette": "Item_Drop.SM.Potion_SM",
-    # "dropSound": "InterfaceSound.Drop_ItemCUE.Drop_PotionCue",
-    # "enchantEnable": false,
-    # "equipSound": "InterfaceSound.Equip_ItemCUE.Equip_NormalCue",
-    # "extractLook": false,
-    # "guildWarehouseStorable": false,
-    # "icon": "Icon_Items.Artisan_Potion_Tex",
-    # "id": 1,
-    # "level": 1,
+    # "name": "service_item_01",
+    # "obtainable": true,
+    # "changeLook": false,
+
+    # dropped - at least for now
+
+    # links only ?
+
     # "linkCrestId": 0,
     # "linkCustomizingId": 0,
     # "linkEquipmentId": 0,
@@ -34,20 +42,24 @@ class ItemData(models.Model):                   #for parsing purposes
     # "linkPetAdultId": 0,
     # "linkPetOrbId": 0,
     # "linkSkillId": 60410500,
+
+    # not usefull
+
+    # slotLimit = models.IntegerField()         Ketoh says useless
+    
+    # "buyPrice": "10000",
+    # "sellPrice": "1000",
+
+    # "changeColorEnable": false,
+    # "destroyable": true,
+    # "extractLook": false,
+
+    # "dropSilhouette": "Item_Drop.SM.Potion_SM",
+    # "sortingNumber": 1101,
     # "masterpieceRate": 0.0,
     # "maxStack": 100,
-    # "name": "service_item_01",
-    # "obtainable": true,
-    # "rank": 0,
-    # "rareGrade": 1,
-    # "requiredEquipmentType": "NO_COMBAT",
-    # "sellPrice": "1000",
-    # "slotLimit": 0,
-    # "sortingNumber": 1101,
-    # "storeSellable": false,
-    # "tradable": false,
-    # "unidentifiedItemGrade": 0,
-    # "warehouseStorable": false
-
-
     # "useOnlyTerritory": false,
+    # "equipSound": "InterfaceSound.Equip_ItemCUE.Equip_NormalCue",
+    # "dropSound": "InterfaceSound.Drop_ItemCUE.Drop_PotionCue",
+    # "coolTime": 30,
+    # "coolTimeGroup": 3,
