3f6f3eb88f4bb1454aa8dc63b336b6b43d63f4a8
kuko0411 <kuko0411@gmail.com>
2017-03-26 18:10:02 +0200

    Changed db backend to PostgreSQL

--- armory/settings.py
+++ armory/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'item'
+    'item',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'armory.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "armory",
+        "USER": "armoryuser",
+        "PASSWORD": "armorypass",
+        "HOST": "127.0.0.1",
+        "PORT": "5433",
     }
 }
 
