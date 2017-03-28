9859e4fdd7f3c0bc34e56c031fe764a15e174cb0
kuko0411 <kuko0411@gmail.com>
2017-03-28 22:34:45 +0200

    Added Item

--- item/views.py
+++ item/views.py
@@ -1,30 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
-from item.models import ItemData
+from item.models import ItemData, Item
 
 import json
 import os.path
 
 # Create your views here.
 def item(request, id):
-    # replace with DB call
-    filepath = os.path.join("items", str(id) + ".json")
-    print(os.path.abspath(filepath))
-
-    content = json.loads(open(filepath).read())
-
-    #replace with DB call
-
-    item_id = content["item"]
-    itemdata = ItemData.objects.get(id = item_id)
+    item = Item.objects.get(id = id)
+    itemdata = ItemData.objects.get(id = item.type)
 
     itemdata.icon = "item/" + itemdata.icon
+
+    itemdata.enchant = item.enchant
+    itemdata.itemLevel = item.itemLevel
+    itemdata.feedstock = item.feedstock
 
     template = loader.get_template("item/tooltip.html")
     return HttpResponse(
         template.render({
             "itemdata": itemdata,
-            "content" : content
         })
     )
