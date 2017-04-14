2fd3fd3801192c4c17da2a391701e90a36940b4c
kuko0411 <kuko0411@gmail.com>
2017-04-14 10:02:53 +0200

    Changed loadbd script

--- loaddb.bat
+++ loaddb.bat
@@ -1,1 +1,4 @@
-python manage.py loaddata fixtures/passivity fixtures/equipdata fixtures/itemdata fixtures/items
+python manage.py loaddata fixtures/passivity 
+python manage.py loaddata fixtures/equipdata 
+python manage.py loaddata fixtures/itemdata 
+python manage.py loaddata fixtures/items
